a76d032c179f9effe88a82ece4ce738a580523d1
Philippe Fanaro <philippefanaro@gmail.com>
2023-05-28 17:54:20 -0300

    more initial boilerplate for the project

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 - [SolidJS](https://www.solidjs.com/)
 - [MDN `Math.random()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
 - [random-js](https://www.npmjs.com/package/random-js)
+- [JavaScript Math.random Normal distribution (Gaussian bell curve)?](https://stackoverflow.com/a/36481059/4756173)
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
 import { Component } from "solid-js";
 
+/*
+ * Standard Normal variate using Box-Muller transform.
+ *
+ * Reference: [JavaScript Math.random Normal distribution (Gaussian bell curve)?](https://stackoverflow.com/a/36481059/4756173)
+ */
+function gaussianRandom(mean: number = 0, stdev: number = 1) {
+  const u = 1 - Math.random(); // Converting [0,1) to (0,1]
+  const v = Math.random();
+  const z = Math.sqrt(-2 * Math.log(u)) * Math.cos(2 * Math.PI * v);
+
+  return z * stdev + mean;
+}
+
 const App: Component = () => {
-  return <h1>Hellow World</h1>;
+  const a = Array.from({ length: 1000 }, () => gaussianRandom());
+
+  return <h1>Hello World</h1>;
 };
 export default App;
