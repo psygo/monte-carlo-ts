164daf0a60dd66cdacdc1a300beb8dba89a0f17f
Philippe Fanaro <philippefanaro@gmail.com>
2023-05-29 10:07:58 -0300

    first version of summing distributions

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { onMount } from "solid-js";
 
+import { Box, Stack, Typography } from "@suid/material";
+
 import {
   Chart,
   ChartData,
@@ -9,6 +11,7 @@ import {
   Title,
   Tooltip,
 } from "chart.js";
+
 import { Bar } from "solid-chartjs";
 
 /*
@@ -27,7 +30,7 @@ function gaussianRandom(mean: number = 0, stdev: number = 1) {
 /**
  * Uses `mean +- 4*stdev` as the width.
  */
-function binGaussianDistribution(
+function binDistribution(
   data: number[],
   mean: number = 0,
   stdev: number = 1,
@@ -57,6 +60,18 @@ function binGaussianDistribution(
   return { bins, binned };
 }
 
+function calcMean(arr: number[]) {
+  return arr.reduce((p, v) => p + v) / arr.length;
+}
+
+function calcStd(arr: number[]) {
+  const n = arr.length;
+  const mean = calcMean(arr);
+  return Math.sqrt(
+    arr.map((x) => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n
+  );
+}
+
 const App = () => {
   /*
    * You must register optional elements before using the chart,
@@ -66,28 +81,103 @@ const App = () => {
     Chart.register(Title, Tooltip, Legend, Colors);
   });
 
-  const [mean, stdev] = [0, 1]
-  const randomData = Array.from({ length: 10_000 }, () => gaussianRandom(mean, stdev));
+  const [mean1, stdev1] = [0, 1];
+  const [mean2, stdev2] = [2, 1];
+  const randomData1 = Array.from({ length: 10_000 }, () =>
+    gaussianRandom(mean1, stdev2)
+  );
+  const randomData2 = Array.from({ length: 10_000 }, () =>
+    gaussianRandom(mean2, stdev2)
+  );
 
-  const { bins, binned } = binGaussianDistribution(randomData, mean, stdev, 100);
+  const randomDataSum = randomData1.map((v, i) => v + randomData2[i]);
+  const randomDataSumMean = calcMean(randomDataSum);
+  const randomDataStd = calcStd(randomDataSum);
 
-  const chartData: ChartData = {
-    labels: bins,
+  const { bins: bins1, binned: binned1 } = binDistribution(
+    randomData1,
+    mean1,
+    stdev1,
+    100
+  );
+  const { bins: bins2, binned: binned2 } = binDistribution(
+    randomData2,
+    mean1,
+    stdev2,
+    100
+  );
+  const { bins: binsSum, binned: binnedSum } = binDistribution(
+    randomDataSum,
+    randomDataSumMean,
+    randomDataStd,
+    100
+  );
+
+  const chartData1: ChartData = {
+    labels: bins1,
     datasets: [
       {
         label: "Bins",
-        data: binned,
+        data: binned1,
+      },
+    ],
+  };
+  const chartData2: ChartData = {
+    labels: bins2,
+    datasets: [
+      {
+        label: "Bins",
+        data: binned2,
+      },
+    ],
+  };
+  const chartDataSum: ChartData = {
+    labels: binsSum,
+    datasets: [
+      {
+        label: "Bins",
+        data: binnedSum,
       },
     ],
   };
 
   const chartOptions: ChartOptions = {
-    responsive: true,
+    responsive: false,
     maintainAspectRatio: false,
   };
 
   return (
-    <Bar data={chartData} options={chartOptions} width={500} height={500} />
+    <Stack>
+      <Box>
+        <Typography>Dist 1</Typography>
+        <Bar
+          data={chartData1}
+          options={chartOptions}
+          width={500}
+          height={500}
+        />
+      </Box>
+
+      <Box>
+        <Typography>Dist 2</Typography>
+        <Bar
+          data={chartData2}
+          options={chartOptions}
+          width={500}
+          height={500}
+        />
+      </Box>
+
+      <Box>
+        <Typography>Dist Sum</Typography>
+        <Bar
+          data={chartDataSum}
+          options={chartOptions}
+          width={500}
+          height={500}
+        />
+      </Box>
+    </Stack>
   );
 };
 export default App;
